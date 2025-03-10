4d6574cc3322ed0aa81ab1c6d8939b6f4fe60d0f
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-03-10 15:13:28 +0600

    add global css

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer"
 
 const App = () => {
     return (
-        <div>
+        <div className="bg-[#23272F] text-gray-200">
             <Navbar />
             <Hero />
             <Infinite />
